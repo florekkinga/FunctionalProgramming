ebb2be521e85916f4a26957ab84208b286fdb85b
florekkinga <florek.kinga@gmail.com>
2020-04-06 10:53:05 +0200

    zad 2 done

--- 03_funkcje_wyzszego_rzedu/02_generatorOperator.hs
+++ 03_funkcje_wyzszego_rzedu/02_generatorOperator.hs
@@ -1,3 +1,4 @@
 generatorOperator :: (lewa -> prawa -> wynik) -> lewa -> (prawa -> wynik)
+generatorOperator = \f x y -> f x y
 -- funkcja przyjmuje operator, który jest typu (lewa -> prawa -> wynik)
 -- oraz lewą część operatora i zwraca funkcję, która przyjmuje prawą część operatora i zwraca wynik

dee00e33ec49f8714e5cd043770cab9c9af18674
florekkinga <florek.kinga@gmail.com>
2020-04-06 10:47:45 +0200

    zad 1 done

--- 03_funkcje_wyzszego_rzedu/01_mojeLiczby.hs
+++ 03_funkcje_wyzszego_rzedu/01_mojeLiczby.hs
@@ -3,9 +3,9 @@ mojeLiczby = [f x | x <- lista, p x]
                    lista = [1..10]          -- lista początkowa
                    p = \b -> b `mod` 2 == 0 -- p wybiera liczby parzyste
  
-mojeLiczby' = -- TUTAJ WPISZ SWOJĄ FUNKCJĘ!
-              -- użyj funkcji wyższego rzędu, np. filter, map, foldl
-              -- spróbuj nie używać nawiasów
+mojeLiczby' =  map f $ filter p lista
              where f = \a -> 2 * a
                    lista = [1..10]
                    p = \b -> b `mod` 2 == 0
+
+
